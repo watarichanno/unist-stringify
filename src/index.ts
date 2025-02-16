81e22c3e52f7228cfe33ec05f04b064bafdfeb16
watarichanno <watarichanno@gmail.com>
2025-02-16 22:34:03 +0700

    Implement serialize algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,65 @@
 import type { Processor } from 'unified';
-import type { Node } from 'unist';
+import type { Literal, Node, Parent } from 'unist';
 
-interface Options {
-  [nodeType: string]: string | ((tree: Node) => string);
+export interface Options {
+  [nodeType: string]: (value: unknown, node: Node) => string;
+}
+
+interface TraversalItem {
+  node: Node;
+  parentItem?: TraversalItem;
+  traversed: boolean;
+  result: string;
+}
+
+function isLiteralNode(node: Node): node is Literal {
+  return 'value' in node;
+}
+
+function isParentNode(node: Node): node is Parent {
+  return 'children' in node;
 }
 
 export function unistToString(tree: Node, handlers: Options): string {
-  return tree.type + ' ' + handlers['o'];
+  const traversalStack: TraversalItem[] = [];
+  traversalStack.push({ node: tree, traversed: false, result: '' });
+
+  while (traversalStack.length > 0) {
+    const item = traversalStack[traversalStack.length - 1];
+    const node = item.node;
+
+    if (isLiteralNode(node)) {
+      if (item.parentItem) {
+        item.parentItem.result += handlers[node.type](node.value, node);
+      } else {
+        return handlers[node.type](node.value, node);
+      }
+      traversalStack.pop();
+    } else if (isParentNode(node)) {
+      if (item.traversed) {
+        if (item.parentItem) {
+          item.parentItem.result += handlers[node.type](item.result, node);
+        } else {
+          return handlers[node.type](item.result, node);
+        }
+        traversalStack.pop();
+      } else {
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          const childNode = node.children[i];
+          traversalStack.push({
+            node: childNode,
+            parentItem: item,
+            traversed: false,
+            result: '',
+          });
+        }
+      }
+    }
+
+    item.traversed = true;
+  }
+
+  return '';
 }
 
 export function unistStringify(this: Processor, options: Options) {
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,28 +3,43 @@ import { u } from 'unist-builder';
 import { unistToString } from '../src/index.js';
 
 test('Literal node with value serializes with value', () => {
-  const tree = u('n', 'a');
-  const result = unistToString(tree, { n: (v) => `[n]${v}[/n]` });
-  expect(result).toBe('[n]a[/n]');
+  const tree = u('l', 'a');
+  const options = { l: (v) => `[l]${v}[/l]` };
+  const result = unistToString(tree, options);
+  expect(result).toBe('[l]a[/l]');
 });
 
 test('Literal node without value serializes with no value', () => {
-  const tree = u('n');
-  const result = unistToString(tree, { n: (v) => `[n]${v}[/n]` });
-  expect(result).toBe('[n][/n]');
+  const tree = u('l');
+  const options = { l: (v) => `[l]${v}[/l]` };
+  const result = unistToString(tree, options);
+  expect(result).toBe('[l][/l]');
 });
 
 test('Parent node with no child serializes with no value', () => {
-  const tree = u('n', []);
-  const result = unistToString(tree, { n: (v) => `[n]${v}[/n]` });
-  expect(result).toBe('[n][/n]');
+  const tree = u('p', []);
+  const options = { p: (v) => `[p]${v}[/p]` };
+  const result = unistToString(tree, options);
+  expect(result).toBe('[p][/p]');
 });
 
-test('Parent node with one literal child serializes with child value', () => {
-  const tree = u('n', [u('m', 'a')]);
-  const result = unistToString(tree, {
-    n: (v) => `[n]${v}[/n]`,
-    m: (v) => `[m]${v}[/m]`,
-  });
-  expect(result).toBe('[n][m]a[/m][/n]');
+test('Parent node with 2 literal childs serializes with 2 child values', () => {
+  const tree = u('p', [u('l1', 'a'), u('l2', 'b')]);
+  const options = {
+    p: (v) => `[p]${v}[/p]`,
+    l1: (v) => `[l1]${v}[/l1]`,
+    l2: (v) => `[l2]${v}[/l2]`,
+  };
+  const result = unistToString(tree, options);
+  expect(result).toBe('[p][l1]a[/l1][l2]b[/l2][/p]');
+});
+
+test('Parent node with 1 parent child serializes with parent child value', () => {
+  const tree = u('p1', [u('p2', [u('l', 'a')])]);
+  const options = {
+    p1: (v) => `[p1]${v}[/p1]`,
+    p2: (v) => `[p2]${v}[/p2]`,
+  };
+  const result = unistToString(tree, options);
+  expect(result).toBe('[p1][p2][l]a[/l][/p2][/p1]');
 });
